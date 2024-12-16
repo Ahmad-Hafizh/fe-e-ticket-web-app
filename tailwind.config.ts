c2b208133f88487a26c6ef77e12b021c0e79ac45
langlangsatrio <langlangsatrio@gmail.com>
2024-12-16 14:18:16 +0700

    install next-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,6 +15,8 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    darkMode: "class",
+    plugins: [nextui()],
   },
   plugins: [],
 } satisfies Config;
