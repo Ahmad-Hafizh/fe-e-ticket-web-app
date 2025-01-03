b34ef375ebd6c1d866eadaa3e59d2019f887c19e
Satrio <langlangsatrio@gmail.com>
2025-01-03 13:10:41 +0700

    Transacation and redux (#12)
    * handle-transaction, sessionstorage
    
    * Add event explorer

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-console.log(process.env.BACKEND_BASE_URL);
+// console.log(process.env.BACKEND_BASE_URL);
 
 export const basicGetApi = axios.create({
-  baseURL: 'https://e-ticket-webapp-minpro.vercel.app',
+  baseURL: "http://localhost:8080",
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
 // import EventCard from '@/components/global-components/EventCard';
 // import RoundedCard from '@/components/global-components/RoundedCard';
-import TopEvent from '../components/views/landing/TopEvent';
-import Jumbotron from '../components/views/landing/Jumbotron';
-import EventCarousel from '../components/views/landing/EventCarousel';
-import Footer from '@/components/global-components/Footer';
-import CarouselRounded from '@/components/global-components/CarouselRounded';
-import Category from '../components/views/landing/Category';
+"use client";
+import TopEvent from "../components/views/landing/TopEvent";
+import Jumbotron from "../components/views/landing/Jumbotron";
+import EventCarousel from "../components/views/landing/EventCarousel";
+import Footer from "@/components/global-components/Footer";
+import CarouselRounded from "@/components/global-components/CarouselRounded";
+import Category from "../components/views/landing/Category";
+import { basicGetApi } from "./config/axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [allEvent, setAllEvent] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const getApi = async () => {
+    try {
+      setIsLoading(true);
+      const response = await basicGetApi.get("/event/all");
+      console.log("ini event all", response.data.result);
+      setAllEvent(response.data.result);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
   return (
     <>
       {/* <div className="bg-gradient-to-tr from-[#FFB457] to-[#FF705B] w-full h-full px-14 md:px-32 lg:px-48 py-20"> */}
@@ -15,7 +37,10 @@ export default function Home() {
         {/**Component Slider Jumbotron */}
         <Jumbotron apicall="/event?cat=Music" />
         {/**Component Slider Event (Event pilihan) */}
-        <EventCarousel label="Event Picked for You" apicall="/event?cat=Music" />
+        <EventCarousel
+          label="Event Picked for You"
+          apicall="/event?cat=Music"
+        />
       </div>
       {/**Component Slider Rounded (kategori) */}
       <div className="bg-blue-900">
@@ -24,7 +49,10 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-white w-full h-full px-10 md:px-32 lg:px-48 py-10 flex flex-col gap-20">
-        <EventCarousel label="Event in your location" apicall="/event?city=Surabaya" />
+        <EventCarousel
+          label="Event in your location"
+          apicall="/event?city=Surabaya"
+        />
       </div>
       <div className="bg-white w-full h-full px-10 md:px-32 lg:px-48 py-10 flex flex-col gap-20">
         <CarouselRounded label="Creator" apicall="/event?city=Surabaya" />
