f47dda6901d73b8f31a5b34a7158b6591cb3c27a
Ahmad-Hafizh <ahaaafizh@gmail.com>
2025-01-09 05:54:00 +0700

    change axios source

--- src/app/config/axios.ts
+++ src/app/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const basicGetApi = axios.create({
-  baseURL: 'http://localhost:3125',
-  // baseURL: 'https://e-ticket-webapp-minpro.vercel.app/',
+  baseURL: 'https://e-ticket-webapp-minpro.vercel.app/',
+  // baseURL: 'http://localhost:3125',
   // baseURL: "http://localhost:8080",
 });
